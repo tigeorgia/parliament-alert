f98c57ac8647bbe3bce58986dfd299f475cc8f3d
Sebastian Henschel <sebastian@transparency.ge>
2011-11-14 11:04:48 +0400

    Define oauth as blank field, so admin doesn't barf at you.

--- apps/registration/extensions/rapidsms/connection.py
+++ apps/registration/extensions/rapidsms/connection.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Change length of identify field
 class ConnectionOAuth(models.Model):
-    oauth = models.CharField(max_length=255,default='')
+    oauth = models.CharField(max_length=255, default='', blank=True)
 
     class Meta:
         abstract = True
