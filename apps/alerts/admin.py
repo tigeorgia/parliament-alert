d66cd0dd1f81f6b5e1ada142f73a9f64a7dacf06
Derek Dohler <derek@transparency.ge>
2012-03-02 13:22:43 +0400

    Added Categories and AlertSendAttempts to Django Admin.

--- apps/alerts/admin.py
+++ apps/alerts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from apps.alerts.models import ParliamentAlert
+from apps.alerts.models import ParliamentAlert, AlertSendAttempt
 
 admin.site.register(ParliamentAlert)
+admin.site.register(AlertSendAttempt)
---
+++ apps/categories/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from apps.categories.models import Category
+
+admin.site.register(Category)
+
