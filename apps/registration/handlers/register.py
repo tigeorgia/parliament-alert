55ad6d22048d28d0544659b2216582a4f9c285b4
Derek Dohler <derek@transparency.ge>
2012-10-29 13:14:38 +0400

    Added DEBUG category that users cannot sign up for.

--- apps/registration/handlers/register.py
+++ apps/registration/handlers/register.py
@@ -59,7 +59,11 @@ class RegisterHandler(KeywordHandler):
                 if cat.matchesKeyword(key):
                     matched_cats.append(cat)
                     cats = cats.exclude(id=cat.id)
-        
+
+        for c in matched_cats: # Don't allow signup for debug.
+            if c.matchesKeyword("DEBUG"):
+                matched_cats.remove(c)
+                break
         if len(matched_cats) == 0:
             cats = Category.objects.all().order_by('?')[:2]
             examples = ""
